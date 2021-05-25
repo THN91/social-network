a1e5ce216894f07bbfb383304efffad4d0735b2e
Taras Honcharuk <taras.goncharuk22@gmail.com>
2021-05-25 22:57:48 +0300

    post get

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,45 +1,23 @@
 import React from "react";
+import * as axios from "axios";
 
 import styless from './users.module.css'
+import userPhoto from '../../assets/image/man-300x300-1.png'
 
 const Users = (props) => {
-
     if (props.users.length === 0) {
-        props.setUsers(
-            [
-                {
-                    id: 1,
-                    photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSdejfG7-Act_jGDn0aunA3XPrLgofw4GDMg&usqp=CAU',
-                    followed: false,
-                    fullName: 'Taras',
-                    status: 'i study at home',
-                    location: {city: 'Dnepr', country: 'Ukraine'}
-                },
-                {
-                    id: 2,
-                    photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSdejfG7-Act_jGDn0aunA3XPrLgofw4GDMg&usqp=CAU',
-                    followed: true,
-                    fullName: 'Dima',
-                    status: 'i work at home',
-                    location: {city: 'Kiev', country: 'Ukraine'}
-                },
-                {
-                    id: 3,
-                    photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSdejfG7-Act_jGDn0aunA3XPrLgofw4GDMg&usqp=CAU',
-                    followed: false,
-                    fullName: 'Oleg',
-                    status: 'busy',
-                    location: {city: 'Odessa', country: 'Ukraine'}
-                }
-            ]
-        );
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {
+                props.setUsers(response.data.items);
+            });
     }
 
     return <div>
         {
             props.users.map(u => <div key={u.id}>
                     <span>
-                        <img src={u.photoUrl} alt="avatar" className={styless.userPhoto}/>
+                        <img src={u.photos.small != null ? u.photos.small : userPhoto} alt="avatar"
+                             className={styless.userPhoto}/>
                        <div>
                            {
                                u.followed
@@ -49,12 +27,12 @@ const Users = (props) => {
                        </div>
                     </span>
                 <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                 <span>
-                        <div>{u.location.city}</div>
-                        <div>{u.location.country}</div>
+                        <div>{"u.location.city"}</div>
+                        <div>{"u.location.country"}</div>
                     </span>
             </div>)
         }
