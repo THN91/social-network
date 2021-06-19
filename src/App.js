f9734c5a3e0364e26a245610f9ab81bd83c8eaf2
Taras Honcharuk <taras.goncharuk22@gmail.com>
2021-06-19 15:45:54 +0300

    login

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {Route} from "react-router-dom";
 
 import './App.css';
-import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
@@ -10,6 +9,7 @@ import Settings from "./components/Settings/Settings";
 import DialogContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
 
 
 
@@ -17,7 +17,7 @@ const App = () => {
 
     return (
         <div className="app-wrapper">
-            <Header/>
+            <HeaderContainer/>
             <Navbar />
             <div className="app-wrapper-content">
                 <Route
