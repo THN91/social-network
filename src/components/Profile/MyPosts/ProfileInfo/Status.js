9e1d6ef6ad601f9f933741a96780416e489ae4c6
Taras Honcharuk <taras.goncharuk22@gmail.com>
2021-07-17 15:54:43 +0300

    componentDidUpdate

--- src/components/Profile/MyPosts/ProfileInfo/Status.js
+++ src/components/Profile/MyPosts/ProfileInfo/Status.js
@@ -29,6 +29,14 @@ class Status extends React.Component {
         })
     };
 
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if(prevProps.status !== this.props.status){
+            this.setState({
+                status: this.props.status
+            })
+        }
+    }
+
     render() {
         return (
             <div>
